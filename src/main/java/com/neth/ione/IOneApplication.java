00a30a4d6b3b67a7d70e18c05ab52c2e47f9c7a1
Nethwindu Gunathilaka <nethwindugunathilaka04@gmail.com>
2025-04-20 15:02:00 +0530

    test commit with intelliJ

--- src/main/java/com/neth/ione/IOneApplication.java
+++ src/main/java/com/neth/ione/IOneApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IOneApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IOneApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(IOneApplication.class, args);
+    }
 
 }
--- src/test/java/com/neth/ione/IOneApplicationTests.java
+++ src/test/java/com/neth/ione/IOneApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class IOneApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
