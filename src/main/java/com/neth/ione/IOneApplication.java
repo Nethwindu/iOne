f65a1b16f6dc7fe555bd8064491be9bd4b5934c6
Nethwindu Gunathilaka <nethwindugunathilaka04@gmail.com>
2025-04-20 14:56:50 +0530

    first commit

---
+++ src/main/java/com/neth/ione/IOneApplication.java
@@ -0,0 +1,13 @@
+package com.neth.ione;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IOneApplication.class, args);
+	}
+
+}
