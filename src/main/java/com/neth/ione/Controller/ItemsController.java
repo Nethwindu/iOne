9b7e22d823027128ada103ca4e33be5ac6ab9757
Nethwindu Gunathilaka <nethwindugunathilaka04@gmail.com>
2025-04-29 21:14:05 +0530

    items page done.

--- src/main/java/com/neth/ione/Controller/ItemsController.java
+++ src/main/java/com/neth/ione/Controller/ItemsController.java
@@ -1,18 +1,54 @@
 package com.neth.ione.Controller;
 
+import com.neth.ione.Model.Items;
 import com.neth.ione.Service.ItemsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/items")
 public class ItemsController {
+
     @Autowired
     private ItemsService itemsService;
 
-    @RequestMapping("/add")
-    public String addNewItem() {
+    // ADD NEW ITEMS PAGE
+    @GetMapping("/add")
+    public String showAddForm(Model model) {
+        model.addAttribute("item", new Items());
         return "add-item";
     }
+
+    // AFTER HITTING SUBMIT
+    @PostMapping("/add")
+    public String addNewItem(@ModelAttribute("item") Items item) {
+        itemsService.saveItem(item);
+        return "redirect:/items/all";
+    }
+
+    // ITEMS PAGE
+    @GetMapping("/all")
+    public String viewItemsList(Model model) {
+        model.addAttribute("itemsList", itemsService.getAllItems());
+        return "items";
+    }
+
+    // EDITING ITEM
+    @GetMapping("/edit/{id}")
+    public String editItem(@PathVariable int id, Model model) {
+        Items item = itemsService.getItemById(id).orElse(null);
+        model.addAttribute("item", item);
+        return "add-item";
+    }
+
+    // DELETING ITEM
+    @GetMapping("/delete/{id}")
+    public String deleteItem(@PathVariable int id) {
+        itemsService.deleteItem(id);
+        return "redirect:/items/all";
+    }
+
+
 }
--- src/main/java/com/neth/ione/Model/Items.java
+++ src/main/java/com/neth/ione/Model/Items.java
@@ -3,28 +3,17 @@ package com.neth.ione.Model;
 import jakarta.persistence.*;
 import lombok.Data;
 
-import java.math.BigDecimal;
-
-@Data
 @Entity
+@Table(name = "items")
+@Data
 public class Items {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @ManyToOne
-    @JoinColumn(name = "model_id", nullable = false)
-    private Model model;
-
-    @ManyToOne
-    @JoinColumn(name = "color_id", nullable = false)
-    private Color color;
-
-    @ManyToOne
-    @JoinColumn(name = "storage_id", nullable = false)
-    private Storage storage;
-
-    @Column(nullable = false)
-    private BigDecimal price;
-//r
+    private String model;
+    private String color;
+    private String storage;
+    private double price;
 }
